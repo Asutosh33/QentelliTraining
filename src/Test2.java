11c55439d94f742decb2e8467003dcf71dc1c2d4
Asutosh <Asutosh@ASUTOSH>
2019-08-09 11:18:50 +0530

    3rd commit

---
+++ src/Test2.java
@@ -0,0 +1,12 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	
+	  @Test
+	  public void  Test2()
+	  {
+		  Reporter.log("Jai mahakal",true);
+	  }
+}
